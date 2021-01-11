28ced91aaee542a5f07f43beb2ea7fe3771a326d
Barratt Isherwood <Barratt Isherwood>
2021-01-11 11:40:07 +0200

    feat(image upload) completed this section

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const postsRoutes = require('./routes/posts');
-
 
 const app = express();
 
@@ -17,6 +17,7 @@ mongoose.connect('mongodb+srv://Admin:FvFJfBTWUTC9SZTw@cluster0.vntqw.mongodb.ne
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
